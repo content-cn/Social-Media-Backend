8f7a014e7d465795c286279279feb8e746d6b87d
CN Studio <cnstudio@CNs-Mac-Studio.local>
2024-05-14 12:15:57 +0530

    Added backend api project

---
+++ config/dbConfig.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+
+async function connectMongoDB() {
+    try {
+        let mongoURI;
+        if (process.env.NODE_ENV === 'production') {
+            mongoURI = process.env.MONGODB_URI_PROD;
+        } else {
+            mongoURI = process.env.MONGODB_URI_DEV;
+        }
+
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("DB connected");
+    } catch (error) {
+        console.log("Was not able to connect to MongoDB", error);
+    }
+}
+
+module.exports = connectMongoDB;
